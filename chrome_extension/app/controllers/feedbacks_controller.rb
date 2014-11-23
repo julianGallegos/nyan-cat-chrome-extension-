de9e45cce152d8eca958efbf7d26f7e4e8d9acb1
julian <julian.gallegos@gmail.com>
2014-11-22 19:46:49 -0800

    added index and show views and controller methods

--- chrome_extension/app/controllers/feedbacks_controller.rb
+++ chrome_extension/app/controllers/feedbacks_controller.rb
@@ -1,9 +1,27 @@
 class FeedbacksController < ApplicationController
 
   def new
+    @feedback = Feedback.new
   end
 
   def create
+    @feedback = Feedback.new(feedback_params)
+
+    @feedback.save
+    redirect_to root_path
   end
+
+  def show
+    @feedback = Feedback.find(params[:id])
+  end
+
+  def index
+    @feedbacks = Feedback.all
+  end
+
+  private
+    def feedback_params
+      params.require(:feedback).permit(:name, :email, :content)
+    end
 
 end
